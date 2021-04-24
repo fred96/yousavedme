01e68a84549973aa9b0c4c479ec738f9b180b279
fred96 <fredmusisi96@gmail.com>
2021-04-24 12:18:44 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to the Tech Guys</div>
+
+                <div class="card-body">
+                
+                        <div class="form-group row">
+                        
+                            <div class="col-md-6">
+                        
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+
+                            <div class="col-md-6">
+                                
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                
+
+                            </div>
+                        </div>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
